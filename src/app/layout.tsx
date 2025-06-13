2e559f5a2cdc858972979435b903c7c420f2f606
Jorge de Souza <jorgedesouzajacintto_jr@hotmail.com>
2025-06-13 18:14:42 -0300

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
+
+export const metadata: Metadata = {
+  title: "Alamo",
+  description: "You can just do things",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="vsc-initialized">
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/ui/app-sidebar.tsx
@@ -0,0 +1,81 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+import Home from "@/assets/vectors/Home.svg";
+import Contact from "@/assets/vectors/Profile.svg";
+import Calendar1 from "@/assets/vectors/Calendar.svg";
+import CircleDollarSign from "@/assets/vectors/Dollar.svg";
+import SquarePen from "@/assets/vectors/Edit.svg";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+
+// Menu items.
+const items = [
+  {
+    title: "Página Inicial",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Clientes",
+    url: "#",
+    icon: Contact,
+  },
+  {
+    title: "Agenda",
+    url: "#",
+    icon: Calendar1,
+  },
+  {
+    title: "Financeiro",
+    url: "#",
+    icon: CircleDollarSign,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>
+            <Avatar>
+              <AvatarImage src="https://i.ibb.co/sv0ZS9qS/background.webp" />
+              <AvatarFallback>MC</AvatarFallback>
+            </Avatar>
+            Marcelo Cavalcante
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon width={24} height={24} />
+                      <span className="font-semibold text-sm text-[var(--primaryTypography)]">
+                        {item.title}
+                      </span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+          <SidebarGroupLabel>
+            <SquarePen width={24} height={24} />
+            <span className="font-semibold text-sm text-[var(--primaryTypography)]">
+              Cadastro
+            </span>
+          </SidebarGroupLabel>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
